bb454e887152939d87a8e05d9f3a56bcb90cebc3
mmusslewhite <168154604+mmusslewhite@users.noreply.github.com>
2024-09-05 18:02:20 -0500

    Update Spawner.cs
    removed unused start function

--- Scripts/Spawner.cs
+++ Scripts/Spawner.cs
@@ -25,13 +25,6 @@ public class Spawner : MonoBehaviour
     {
         CalculateWeights();
     }
-   
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update() {
